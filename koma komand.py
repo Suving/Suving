f6363113687875171a058f6f32b18c41421ba729
Suving <93243033+Suving@users.noreply.github.com>
2021-11-25 13:17:08 +0200

    koma parandamis komand

---
+++ koma komand.py
@@ -0,0 +1,4 @@
+lause = "Osaühing Giga Investeeringud sai Tartu linnavalitsuselt loa et Turu tänaval vana keskkatlamaja kontorihoone ja selle juures asuva kaarhalli lammutamise loa."
+
+lause = lause.replace(" et ", ", et ")
+print(lause)
